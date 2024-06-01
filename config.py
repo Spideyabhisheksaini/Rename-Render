b819e9c6cc6f20c67de2fd0cb2c765f0d8b1e844
Spideyabhisheksaini <sainiabhi310@gmail.com>
2024-06-01 15:22:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ API_HASH = os.environ.get("API_HASH", "7be1392c2fe5ebf4fc3228706fbfb504")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6570108244:AAFogHLTqoj2FOWwNYxe70ka8t_lDLo6eyI") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "Movies_Telegram_Files") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "@MovieTimes_TV") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "Missminutes")     
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Tonystarkbotz:dgRUcv8OktóDazpd@cluster0.pg5q3xr.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Tonystarkbotz:Tonystarkbotz@missminutes.lbzdtkj.mongodb.net/?retryWrites=true&w=majority&appName=Missminutes")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
