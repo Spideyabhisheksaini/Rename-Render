d2c9650bd7e801eb02fc05e8411ee90aa84fde0b
Spideyabhisheksaini <sainiabhi310@gmail.com>
2024-06-02 07:55:51 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -8,30 +8,29 @@ async def refunc(client, message):
     if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
         new_name = message.text
         await message.delete()
-
+        
         msg = await client.get_messages(message.chat.id, reply_message.id)
         file = msg.reply_to_message
         media = getattr(file, file.media.value)
-
+        
         if not "." in new_name:
             if "." in media.file_name:
                 extn = media.file_name.rsplit('.', 1)[-1]
             else:
                 extn = "mkv"
             new_name = new_name + "." + extn
-
+        
         await reply_message.delete()
-
+        
         buttons = [[InlineKeyboardButton("📁 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃𝚂", callback_data="upload_document")]]
-
+        
         if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
             buttons.append([InlineKeyboardButton("🎥 𝚅𝙸𝙳𝙴𝙾", callback_data="upload_video")])
         elif file.media == MessageMediaType.AUDIO:
             buttons.append([InlineKeyboardButton("🎵 𝙰𝚄𝙳𝙸𝙾", callback_data="upload_audio")])
-
+        
         await message.reply_text(
-            f"**Select the output file type**\n**New Name ->** ` {new_name} `",
+            f"**Select the output file type**\n**New Name ->** :- `{new_name}`",
             reply_to_message_id=file.id,
-            reply_markup=InlineKeyboardMarkup(buttons),
-            parse_mode="Markdown"
+            reply_markup=InlineKeyboardMarkup(buttons)
         )
